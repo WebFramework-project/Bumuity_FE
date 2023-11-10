ae960bc3500908a83ccfee99a8cc928367c3b320
ssarisong <thdtnwls2989@naver.com>
2023-11-10 13:27:29 +0900

    :lipstick: 메시지 입력창 추가

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -1,7 +1,8 @@
+import React, { useState } from "react";
 import "../styles/ChatRoom.css";
 
 const ChatRoom = ({ roomId, currentUser }) => {
-  const messages = [
+  const [messages, setMessages] = useState([
     {
       id: 1,
       name: "김김김",
@@ -34,38 +35,72 @@ const ChatRoom = ({ roomId, currentUser }) => {
       time: "18:15",
       user: true,
     },
-  ];
+  ]);
+
+  const [newMessage, setNewMessage] = useState("");
+
+  const sendMessage = () => {
+    if (newMessage.trim()) {
+      const newMessageObj = {
+        id: messages.length + 1,
+        name: currentUser.name,
+        title: currentUser.title,
+        text: newMessage,
+        time: new Date().toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        user: true,
+      };
+      setMessages([...messages, newMessageObj]);
+      setNewMessage("");
+    }
+  };
 
   const sortedMessages = messages.sort((a, b) => a.time.localeCompare(b.time));
 
   return (
     <div className="chat-room">
-      {sortedMessages.map((message) => (
-        <div
-          key={message.id}
-          className={`message ${message.user ? "user" : "other"}`}
-        >
-          {!message.user && (
-            <div className="message-header">
-              <img
-                src="https://w7.pngwing.com/pngs/627/693/png-transparent-computer-icons-user-user-icon-thumbnail.png"
-                alt={`${message.name}`}
-                className="message-image"
-              />
-              <span className="message-name">{message.name}</span>
-              {message.title && (
-                <span className="message-title">{message.title}</span>
-              )}
+      <div className="messages-container">
+        {sortedMessages.map((message) => (
+          <div
+            key={message.id}
+            className={`message ${message.user ? "user" : "other"}`}
+          >
+            {!message.user && (
+              <div className="message-header">
+                <img
+                  src="https://w7.pngwing.com/pngs/627/693/png-transparent-computer-icons-user-user-icon-thumbnail.png"
+                  alt={`${message.name}`}
+                  className="message-image"
+                />
+                <span className="message-name">{message.name}</span>
+                {message.title && (
+                  <span className="message-title">{message.title}</span>
+                )}
+              </div>
+            )}
+            <div className={`message-content ${message.user ? "user" : ""}`}>
+              <div className="message-bubble">
+                <div className="message-text">{message.text}</div>
+              </div>
+              <span className="message-time">{message.time}</span>
             </div>
-          )}
-          <div className={`message-content ${message.user ? "user" : ""}`}>
-            <div className="message-bubble">
-              <div className="message-text">{message.text}</div>
-            </div>
-            <span className="message-time">{message.time}</span>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className="message-input-container">
+        <input
+          type="text"
+          className="message-input"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder="메시지를 입력하세요"
+        />
+        <button onClick={sendMessage} className="send-message-button">
+          전송
+        </button>
+      </div>
     </div>
   );
 };
