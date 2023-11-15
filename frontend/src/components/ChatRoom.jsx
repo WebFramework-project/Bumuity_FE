be895c609d2f7c5ec47bd34ba9ab347e9bc144ef
ssarisong <thdtnwls2989@naver.com>
2023-11-15 19:06:38 +0900

    ChatRoom에 react-chat-elements 적용

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { MessageBox, Input, Button } from "react-chat-elements";
 import ScrollToBottom from "react-scroll-to-bottom";
 import "../styles/ChatRoom.css";
 
@@ -9,32 +10,28 @@ const ChatRoom = ({ roomId, currentUser }) => {
       name: "김김김",
       title: "사장",
       text: "오늘 저녁은 뭘 먹을지 회의를 시작해보지",
-      time: "18:00",
-      user: false,
+      time: new Date(2023, 10, 12, 12, 0),
     },
     {
       id: 2,
       name: "이이이",
       title: "상무",
       text: "돈까스만 아니면 될 것 같습니다.",
-      time: "18:05",
-      user: false,
+      time: new Date(2023, 10, 12, 12, 5),
     },
     {
       id: 3,
       name: "박박박",
       title: "",
       text: "그럼 카레로 하죠",
-      time: "18:10",
-      user: false,
+      time: new Date(2023, 10, 12, 12, 10),
     },
     {
       id: 4,
       name: currentUser.name,
       title: currentUser.title,
       text: "좋습니다.",
-      time: "18:15",
-      user: true,
+      time: new Date(2023, 10, 12, 12, 15),
     },
   ]);
 
@@ -67,50 +64,71 @@ const ChatRoom = ({ roomId, currentUser }) => {
     }
   };
 
-  const sortedMessages = messages.sort((a, b) => a.time.localeCompare(b.time));
-
   return (
     <div className="chat-room">
       <ScrollToBottom className="messages-container">
-        {sortedMessages.map((message) => (
+        {messages.map((message) => (
           <div
             key={message.id}
-            className={`message ${message.user ? "user" : "other"}`}
+            className={`message ${
+              message.name === currentUser.name ? "user" : "other"
+            }`}
           >
-            {!message.user && (
-              <div className="message-header">
+            {message.name !== currentUser.name && (
+              <div className="message-sender-info">
                 <img
                   src="https://w7.pngwing.com/pngs/627/693/png-transparent-computer-icons-user-user-icon-thumbnail.png"
                   alt={`${message.name}`}
-                  className="message-image"
+                  className="message-sender-image"
                 />
-                <span className="message-name">{message.name}</span>
+                <span className="message-sender-name">{message.name}</span>
                 {message.title && (
-                  <span className="message-title">{message.title}</span>
+                  <span className="message-sender-title">{message.title}</span>
                 )}
               </div>
             )}
-            <div className={`message-content ${message.user ? "user" : ""}`}>
-              <div className="message-bubble">
-                <div className="message-text">{message.text}</div>
+            {message.name === currentUser.name && (
+              <div className="message-sender-info">
+                <span className="message-sender-name">&nbsp;</span>
+                {message.title && (
+                  <span className="message-sender-title">&nbsp;</span>
+                )}
               </div>
-              <span className="message-time">{message.time}</span>
-            </div>
+            )}
+            <MessageBox
+              style={{
+                backgroundColor:
+                  message.name === currentUser.name ? "blue" : "white",
+                color: message.name === currentUser.name ? "white" : "black", // 텍스트 색상도 조절할 수 있습니다.
+              }}
+              position={message.name === currentUser.name ? "right" : "left"}
+              type={"text"}
+              text={message.text}
+              date={message.time}
+            />
           </div>
         ))}
-        <div ref={messagesEndRef}></div>
+        <div ref={messagesEndRef}/>
       </ScrollToBottom>
       <div className="message-input-container">
-        <input
-          type="text"
-          className="message-input"
+        <Input
+          placeholder="메시지를 입력하세요"
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
-          placeholder="메시지를 입력하세요"
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              sendMessage();
+            }
+          }}
+          rightButtons={
+            <Button
+              color="white"
+              backgroundColor="blue"
+              text="전송"
+              onClick={sendMessage}
+            />
+          }
         />
-        <button onClick={sendMessage} className="send-message-button">
-          전송
-        </button>
       </div>
     </div>
   );
