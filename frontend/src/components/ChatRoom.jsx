857fa507dbfe18aaf7c812d0f3569b954a3102af
ssarisong <thdtnwls2989@naver.com>
2023-11-16 21:25:49 +0900

    채팅방별 채팅 구현

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -7,31 +7,107 @@ const ChatRoom = ({ roomId, currentUser }) => {
   const [messages, setMessages] = useState([
     {
       id: 1,
-      name: "김김김",
-      title: "사장",
+      chatRoomId: 1,
+      name: "이재용",
+      title: "회장",
       text: "오늘 저녁은 뭘 먹을지 회의를 시작해보지",
-      time: new Date(2023, 10, 12, 12, 0),
+      time: new Date(2023, 10, 15, 12, 0),
     },
     {
       id: 2,
-      name: "이이이",
-      title: "상무",
-      text: "돈까스만 아니면 될 것 같습니다.",
-      time: new Date(2023, 10, 12, 12, 5),
+      chatRoomId: 4,
+      name: "이해진",
+      title: "대표",
+      text: "다들 네이버페이 쓰자~",
+      time: new Date(2023, 10, 15, 9, 0),
     },
     {
       id: 3,
-      name: "박박박",
-      title: "",
-      text: "그럼 카레로 하죠",
-      time: new Date(2023, 10, 12, 12, 10),
+      chatRoomId: 2,
+      name: "최부기",
+      title: "이사",
+      text: "내일 회식은 롯데리아에서 하겠습니다.",
+      time: new Date(2023, 10, 15, 3, 0),
     },
     {
       id: 4,
+      chatRoomId: 1,
+      name: "이한성",
+      title: "상무",
+      text: "돈까스만 아니면 될 것 같습니다.",
+      time: new Date(2023, 10, 15, 12, 5),
+    },
+    {
+      id: 5,
+      chatRoomId: 4,
+      name: currentUser.name,
+      title: currentUser.title,
+      text: "넵",
+      time: new Date(2023, 10, 15, 9, 2),
+    },
+    {
+      id: 6,
+      chatRoomId: 1,
+      name: "박상상",
+      title: "",
+      text: "그럼 카레로 하죠",
+      time: new Date(2023, 10, 15, 12, 10),
+    },
+    {
+      id: 7,
+      chatRoomId: 3,
+      name: "이승건",
+      title: "대표",
+      text: "토스 짱짱",
+      time: new Date(2023, 10, 15, 10, 0),
+    },
+    {
+      id: 8,
+      chatRoomId: 2,
+      name: "김범수",
+      title: "사장",
+      text: "누가 새벽 3시에 카톡을 보내냐",
+      time: new Date(2023, 10, 15, 3, 5),
+    },
+    {
+      id: 9,
+      chatRoomId: 3,
+      name: currentUser.name,
+      title: currentUser.title,
+      text: "토스 짱짱짱!!",
+      time: new Date(2023, 10, 15, 10, 0),
+    },
+    {
+      id: 10,
+      chatRoomId: 1,
       name: currentUser.name,
       title: currentUser.title,
       text: "좋습니다.",
-      time: new Date(2023, 10, 12, 12, 15),
+      time: new Date(2023, 10, 15, 12, 15),
+    },
+    {
+      id: 11,
+      chatRoomId: 2,
+      name: "최부기",
+      title: "이사",
+      text: "죄송합니다.",
+      time: new Date(2023, 10, 15, 3, 5),
+    },
+    {
+      id: 12,
+      chatRoomId: 3,
+      name: "곽미래",
+      title: "사원",
+      text: "토스 짱짱짱!!",
+      time: new Date(2023, 10, 15, 10, 0),
+    },
+    {
+      id: 13,
+      chatRoomId: 4,
+      name: "부공학",
+      title: "대리",
+      text: "포인트좀 뿌려주세요",
+      time: new Date(2023, 10, 15, 9, 5),
     },
   ]);
 
@@ -39,6 +115,7 @@ const ChatRoom = ({ roomId, currentUser }) => {
   const messagesEndRef = useRef(null);
 
   useEffect(() => {
+
     scrollToBottom();
   }, [messages]);
 
@@ -50,6 +127,7 @@ const ChatRoom = ({ roomId, currentUser }) => {
     if (newMessage.trim()) {
       const newMessageObj = {
         id: messages.length + 1,
+        chatRoomId: roomId,
         name: currentUser.name,
         title: currentUser.title,
         text: newMessage,
@@ -67,48 +145,50 @@ const ChatRoom = ({ roomId, currentUser }) => {
   return (
     <div className="chat-room">
       <ScrollToBottom className="messages-container">
-        {messages.map((message) => (
-          <div
-            key={message.id}
-            className={`message ${
-              message.name === currentUser.name ? "user" : "other"
-            }`}
-          >
-            {message.name !== currentUser.name && (
-              <div className="message-sender-info">
-                <img
-                  src="https://w7.pngwing.com/pngs/627/693/png-transparent-computer-icons-user-user-icon-thumbnail.png"
-                  alt={`${message.name}`}
-                  className="message-sender-image"
-                />
-                <span className="message-sender-name">{message.name}</span>
-                {message.title && (
-                  <span className="message-sender-title">{message.title}</span>
-                )}
-              </div>
-            )}
-            {message.name === currentUser.name && (
-              <div className="message-sender-info">
-                <span className="message-sender-name">&nbsp;</span>
-                {message.title && (
-                  <span className="message-sender-title">&nbsp;</span>
-                )}
-              </div>
-            )}
-            <MessageBox
-              style={{
-                backgroundColor:
-                  message.name === currentUser.name ? "blue" : "white",
-                color: message.name === currentUser.name ? "white" : "black", // 텍스트 색상도 조절할 수 있습니다.
-              }}
-              position={message.name === currentUser.name ? "right" : "left"}
-              type={"text"}
-              text={message.text}
-              date={message.time}
-            />
-          </div>
-        ))}
-        <div ref={messagesEndRef}/>
+        {messages
+          .filter(message => message.chatRoomId === roomId)
+          .sort((a, b) => new Date(a.time) - new Date(b.time))
+          .map((message) => (
+            <div
+              key={message.id}
+              className={`message ${message.name === currentUser.name ? "user" : "other"
+                }`}
+            >
+              {message.name !== currentUser.name && (
+                <div className="message-sender-info">
+                  <img
+                    src="https://w7.pngwing.com/pngs/627/693/png-transparent-computer-icons-user-user-icon-thumbnail.png"
+                    alt={`${message.name}`}
+                    className="message-sender-image"
+                  />
+                  <span className="message-sender-name">{message.name}</span>
+                  {message.title && (
+                    <span className="message-sender-title">{message.title}</span>
+                  )}
+                </div>
+              )}
+              {message.name === currentUser.name && (
+                <div className="message-sender-info">
+                  <span className="message-sender-name">&nbsp;</span>
+                  {message.title && (
+                    <span className="message-sender-title">&nbsp;</span>
+                  )}
+                </div>
+              )}
+              <MessageBox
+                style={{
+                  backgroundColor:
+                    message.name === currentUser.name ? "blue" : "white",
+                  color: message.name === currentUser.name ? "white" : "black", // 텍스트 색상도 조절할 수 있습니다.
+                }}
+                position={message.name === currentUser.name ? "right" : "left"}
+                type={"text"}
+                text={message.text}
+                date={message.time}
+              />
+            </div>
+          ))}
+        <div ref={messagesEndRef} />
       </ScrollToBottom>
       <div className="message-input-container">
         <Input
