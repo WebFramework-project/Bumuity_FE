c2c545aeec8892f1c740a82fb282df8b5947ad80
dain0l <123536537+dain0l@users.noreply.github.com>
2023-11-20 00:40:13 +0900

    todolist수정

--- frontend/src/components/todo/TodoMain.jsx
+++ frontend/src/components/todo/TodoMain.jsx
@@ -1,50 +1,104 @@
-import TodoList from "./TodoList";
-import TodoItem from "./TodoList";
+import React, { useState } from 'react';
 
 function TodoMain() {
-    const backgroundStyle = {
-      backgroundColor: "#3B374E",
-      width: "95%",
-      height: "48vh", 
-      marginTop:"13px",
-      marginLeft:"10px",
-      overflow: "hidden"
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { text: newTask, completed: false }]);
+      setNewTask('');
     }
-  
-    const todoStyle = {
-        fontSize: "18px", 
+  };
+
+  const handleToggle = (index) => {
+    const updatedTasks = [...tasks];
+    const toggledTask = updatedTasks.splice(index, 1)[0];
+    toggledTask.completed = !toggledTask.completed;
+    
+    updatedTasks.push(toggledTask);
+    setTasks(updatedTasks);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      addTask();
+    }
+  };
+
+  return (
+    <div style={{ flexDirection: "column", alignItems: "center" }}>
+      <div style={{
+        fontSize: "18px",
         fontFamily: "Arial, sans-serif",
-        fontWeight: "bold", 
+        fontWeight: "bold",
         marginBottom: "10px",
         textAlign: "center",
-        border: "5px solid #fff", 
-        padding: "10px",
-        borderRadius: "8px", 
+        border: "5px solid #fff",
+        padding: "8px",
+        borderRadius: "8px",
         color: "lightgray",
-      };
-
-    const todobackgroundStyle = {
-        backgroundColor: "lightgray",
-        width: "95%",
-        height: "40vh", 
-        marginTop:"13px",
-        marginLeft:"10px",
-        overflow: "hidden"
-
-    }
-
-
-    return (
-      <div style={backgroundStyle}>
-        <div style ={todoStyle}>TO DO LIST</div>
-        <div style ={todobackgroundStyle}></div>
-        <div>< TodoList/></div>
-    
-        
+        backgroundColor: "#3B374E",
+        width: "93%",
+      }}>
+        TO DO LIST
       </div>
 
-    );
-  }
-  
-  export default TodoMain;
-  
+    
+      <div style={{ 
+        flexDirection: "column", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        backgroundColor: "lightgrey", 
+        marginBottom: "10px",
+        marginLeft: "10px",
+        
+        width: "100%", 
+        height: "5vh", 
+        paddingTop: "15px", 
+        paddingLeft: "6px", 
+        paddingRight: "50px", 
+        overflow: "hidden", 
+        borderRadius: "10px", 
+        
+        }}>
+        <input
+          type="text"
+          value={newTask} 
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Enter a new task"
+          style={{ width: "80%", marginLeft: "15px", fontSize: "15px" }}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={addTask}>+</button>
+      </div>
+
+
+      <div style={{ 
+        backgroundColor: "lightgrey", 
+        width: "100%", 
+        height: "37vh", 
+        marginLeft: "10px",
+        marginRight: "10px",
+        overflow: "hidden", 
+        display: "flex", 
+        flexDirection: "column", 
+        paddingTop: "15px", 
+        paddingLeft: "10px",
+        flexWrap: "wrap", 
+        justifyContent: "flex-start",
+        alignItems: "left",
+        borderRadius: "10px"
+      }}>
+         
+        {tasks.map((task, index) => (
+          <div key={index} onClick={() => handleToggle(index)}>
+            {task.completed ? '✅' : '◻️'} {task.text}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TodoMain;
