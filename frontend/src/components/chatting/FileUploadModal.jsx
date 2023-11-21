9a320ec3909aa2abfb07dabc0971da1546a56390
ssarisong <thdtnwls2989@naver.com>
2023-11-21 13:00:34 +0900

    :hammer: 채팅에 이미지 전송 구현

--- frontend/src/components/chatting/FileUploadModal.jsx
+++ frontend/src/components/chatting/FileUploadModal.jsx
@@ -1,29 +1,42 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React, { useState } from "react";
+import Modal from "react-modal";
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    width: '300px',
-    height: '200px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    border: '1px solid #ccc',
-    borderRadius: '5px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-    backgroundColor: '#fff',
-    padding: '20px',
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "300px",
+    height: "200px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+    backgroundColor: "#fff",
+    padding: "20px",
   },
 };
 
-const FileUploadModal = ({ isOpen, onRequestClose }) => {
+const FileUploadModal = ({ isOpen, onRequestClose, onFileUpload }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = () => {
+    if (selectedFile) {
+      onFileUpload(selectedFile);
+      onRequestClose();
+    }
+  };
+
   return (
     <Modal
       isOpen={isOpen}
@@ -31,9 +44,13 @@ const FileUploadModal = ({ isOpen, onRequestClose }) => {
       style={customStyles}
       ariaHideApp={false}
     >
-      <h2 style={{ marginBottom: '20px' }}>이미지 전송</h2>
-      <input type="file" style={{ marginBottom: '20px' }} />
-      <button onClick={onRequestClose}>전송하기</button>
+      <h2 style={{ marginBottom: "20px" }}>이미지 전송</h2>
+      <input
+        type="file"
+        onChange={handleFileChange}
+        style={{ marginBottom: "20px" }}
+      />
+      <button onClick={handleFileUpload}>전송하기</button>
     </Modal>
   );
 };
