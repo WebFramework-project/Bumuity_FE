2651bb04d29d88720666b8ae1b4d474725617963
Sengoo <lesley023@naver.com>
2023-11-21 16:40:00 +0900

    프로필사진 등록 수정

--- frontend/src/components/userProfile/UserPic.jsx
+++ frontend/src/components/userProfile/UserPic.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import iconImage from '../../images/start/profilesetIcon.png';
 
@@ -9,31 +8,54 @@ function UserPic() {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
-            reader.onloadend = () => {
-                setSelectedProfileImage(reader.result);
+            reader.onload = (e) => {
+                const img = new Image();
+                img.onload = () => {
+                    const maxWidth = 10.5 * 16; // 최대 가로 -> rem 픽셀로 변경
+                    const maxHeight = 9.75 * 16; // 최대 세로도 변경
+                    let width = img.width;
+                    let height = img.height;
+
+                    if (width > maxWidth || height > maxHeight) {
+                        const ratio = Math.min(maxWidth / width, maxHeight / height);
+                        width *= ratio;
+                        height *= ratio;
+                    }
+
+                    const canvas = document.createElement('canvas'); //canvas API 사용
+                    canvas.width = width;
+                    canvas.height = height;
+
+                    const ctx = canvas.getContext('2d');
+                    ctx.drawImage(img, 0, 0, width, height);
+
+                    const resizedImage = canvas.toDataURL('image/jpeg');
+                    setSelectedProfileImage(resizedImage);
+                };
+                img.src = e.target.result;
             };
             reader.readAsDataURL(file);
         }
     };
 
     const containerStyle = {
-        marginTop: "0.5rem",
-        display: "flex",
-        justifyContent: "center",
-        marginBottom: "0.5rem",
-        alignItems: "center",
+        marginTop: '0.5rem',
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: '0.5rem',
+        alignItems: 'center',
     };
 
     const profilesetStyle = {
-        backgroundImage: `url(${selectedProfileImage})`,
-        backgroundSize: "10.5rem 9.75rem",
-        backgroundPosition: "center",
-        width: "10.5rem",
-        height: "9.75rem",
-        flexShrink: "0",
-        border: "1px solid #322275",
-        background: "#0A0A14",
-        borderRadius: "50%",
+        backgroundImage: selectedProfileImage ? `url(${selectedProfileImage})` : 'none',
+        backgroundSize: '10.5rem 9.75rem', // '10.5rem 9.75rem' 등으로 설정하시면 됩니다.
+        backgroundPosition: 'center',
+        width: '10.5rem',
+        height: '9.75rem',
+        flexShrink: '0',
+        border: '1px solid #322275',
+        background: '#0A0A14',
+        borderRadius: '50%',
     };
 
     const iconStyle = {
